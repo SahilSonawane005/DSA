7be3d4fb19e37a5e795dadb6de0f5b12271c2ebb
sahil <sahilofficial0005@gmail.com>
2024-08-12 10:54:59 +0530

    This file include string inbuilt methods, functions like revserse String & checking if string is palindrom or not

--- Topics/String/StringBasics.java
+++ Topics/String/StringBasics.java
@@ -1,5 +1,7 @@
 package String;
 
+import java.util.Objects;
+
 public class StringBasics {
     public static void main(String[] args) {
         String s = "hello";
@@ -14,5 +16,34 @@ public class StringBasics {
         System.out.println(str.append("!!"));
         System.out.println(str.indexOf("!"));
         System.out.println(str.isEmpty());
+
+        String s2 = "abcdefgh"; //orignal string
+        System.out.println(s2);
+        System.out.println(revStr(s2));  //revserse string
+
+        String s3 = "aba";
+        System.out.println(revStr(s3));
+        System.out.println(isPalindrom(s3));
+
+    }
+    public static String revStr(String s){
+//        first way to revserse string
+//        String newStr = "";
+//        for (int i = s.length()-1; i >=0 ; i--) {
+//            char ch = s.charAt(i);
+//            newStr = newStr + ch;
+//        }
+
+//        string revserse using stringBuilder
+        StringBuilder newStr = new StringBuilder();
+        for (int i = s.length()-1; i >=0 ; i--) {
+            char ch = s.charAt(i);
+            newStr.append(ch);
+        }
+        return newStr.toString();
+    }
+
+    public static boolean isPalindrom(String s){
+        return Objects.equals(s, revStr(s));
     }
 }
